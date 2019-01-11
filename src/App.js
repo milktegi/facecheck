7af0a93962a970bf6413daafa2af42b79e0c71fe
milktegi <milkteegi@gmail.com>
2019-01-11 14:06:59 +0900

    hooked up the Clarafai API and displayed image

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ShowImage from './components/ShowImage/ShowImage';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '3c37a4ac169b4c5f837b1a572be63bdd'
+});
 
 const particlesOption = {
   particles: {
@@ -13,14 +19,43 @@ const particlesOption = {
       density: {
         enable: true,
         value_area: 800
-      },
-  
-    },
- 
+      }
+    }
   }
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    };
+  }
+
+  onInputChange = event => {
+    // c.log(event.target.value);
+    this.setState({
+      input: event.target.value
+    });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    console.log('clicked!');
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        // do something with response
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
@@ -28,9 +63,12 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ShowImage />
+        <ShowImage
+          onButtonSubmit={this.onButtonSubmit}
+          onInputChange={this.onInputChange}
+        />
 
-        {/* <FaceRecognition/> */}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/ShowImage/ShowImage.js
+++ src/components/ShowImage/ShowImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ShowImage.css'
 
-const ShowImage = () => {
+const ShowImage = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className="f4">
@@ -10,10 +10,12 @@ const ShowImage = () => {
 			<div className="center">
 			<div className="form center pa4 br3 shadow-5">
 				<input 
+				onChange={onInputChange}
 				className="f4 pa2 w-70 center"
 				type="tex"
 				/>
 				<button
+				onClick={onButtonSubmit}
 				className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
 				>Detect</button>
 			</div>
