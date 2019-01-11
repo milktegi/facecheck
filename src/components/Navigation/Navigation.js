9b376e456e19d769cdc2899b8c75670006e4713d
milktegi <milkteegi@gmail.com>
2019-01-11 12:49:55 +0900

    completed basic layout with no function

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<nav style={{ display: 'flex', justifyContent: 'flex-end'}}>
+			<p className="f4 link dim black pa3 pointer">로그아웃</p>
+		</nav>
+	)
+}
+
+export default Navigation;
