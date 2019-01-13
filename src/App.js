1adc3e8468e79b65b8bc7a14fc115ec85a37f87f
milktegi <milkteegi@gmail.com>
2019-01-13 09:49:19 +0900

    added routing

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Logo from './components/Logo/Logo';
 import ShowImage from './components/ShowImage/ShowImage';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
+import Register from './components/Register/Register';
 import './App.css';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
+import Signin from './components/Signin/Signin';
 
 const app = new Clarifai.App({
   apiKey: '3c37a4ac169b4c5f837b1a572be63bdd'
@@ -29,10 +31,21 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     };
   }
 
+  onRouteChange = route => {
+    if(route === 'signout'){
+      this.setState({ isSignedIn: false })
+    } else if(route === 'home'){
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
   onInputChange = event => {
     // c.log(event.target.value);
     this.setState({
@@ -43,32 +56,61 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     console.log('clicked!');
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        // do something with response
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch(err => console.log(err));
+  };
+
+  // 얼굴 계산 위치 함수
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box: box });
   };
 
   render() {
+   
+    const { isSignedIn, imageUrl, route, box } = this.state; 
+
     return (
       <div className="App">
         <Particles className="particles" params={particlesOption} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ShowImage
-          onButtonSubmit={this.onButtonSubmit}
-          onInputChange={this.onInputChange}
-        />
-
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === 'home' ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ShowImage
+              onButtonSubmit={this.onButtonSubmit}
+              onInputChange={this.onInputChange}
+            />
+            <FaceRecognition
+              box={box}
+              imageUrl={imageUrl}
+            />
+          </div>
+        ) : route === 'signin' ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './FaceRecognition.css'
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
 	return (
 		<div className="center ma">
 			<div className="absolute mt5">
-			<img src={imageUrl} alt="" width="500px" height="auto"/>
+			<img 
+			id="inputimage"
+			src={imageUrl} alt="" width="500px" height="auto"/>
+			<div 
+			style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}
+			className="bounding-box"></div>
 			</div>
 
 		</div>
